c184fbb22561c70059b2bab45a489d5a93a8c1bc
pinkmustard <wjddus7431@gmail.com>
2023-11-06 23:21:30 +0900

    대문 페이지와 자기소개 만들기

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+#landing.html을 보여줌
+def landing(request):
+    return render(
+        request,
+        'single_pages/landing.html'
+    )
 
-# Create your views here.
+# about_me.html을 보여줌
+def about_me(request):
+    return render(
+        request,
+        'single_pages/about_me.html'
+    )
